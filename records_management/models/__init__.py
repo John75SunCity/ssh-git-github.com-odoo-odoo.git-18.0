6c7bbcfeb772332c540fa32276ab60a854513b7b
John75SunCity <invoices@suncityboxes.com>
2025-06-19 09:43:10 +0000

    Describe your changes

--- records_management/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import scrm_records_management
--- records_management/models/stock_production_lot.py
+++ records_management/models/stock_production_lot.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models  # type: ignore
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
