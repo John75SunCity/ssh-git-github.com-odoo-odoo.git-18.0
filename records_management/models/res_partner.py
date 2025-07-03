b9d72180f5a17a46f58355b6cbcc935c681de997
John Cope <invoices@suncityboxes.com>
2025-07-03 00:24:37 -0600

    Create res_partner.py
    Create res_partner.py

---
+++ records_management/models/res_partner.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    document_ids = fields.One2many('records.document', 'partner_id', string='Related Documents')
+    document_count = fields.Integer(compute='_compute_document_count')
+    
+    def _compute_document_count(self):
+        for partner in self:
+            partner.document_count = len(partner.document_ids)
+            
+    def action_view_documents(self):
+        self.ensure_one()
+        return {
+            'name': 'Documents',
+            'type': 'ir.actions.act_window',
+            'res_model': 'records.document',
+            'view_mode': 'tree,form',
+            'domain': [('partner_id', '=', self.id)],
+            'context': {'default_partner_id': self.id},
+        }
