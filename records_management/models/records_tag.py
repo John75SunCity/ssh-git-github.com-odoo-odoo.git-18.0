c9012e27994f7e27efed07a8817756fa90da7681
John Cope <invoices@suncityboxes.com>
2025-07-22 00:01:53 +0000

    MINIMAL: Remove type ignore comment from import statement in records.tag model

--- records_management/models/records_tag.py
+++ records_management/models/records_tag.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api # type: ignore
+from odoo import models, fields, api
 
 
 class RecordsTag(models.Model):
