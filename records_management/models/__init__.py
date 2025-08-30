d9f914e45e4de09fe48698eb36a7d68277177f4d
John75SunCity <johncope75@yahoo.com>
2025-08-30 02:44:37 -0600

    fix: Add missing model imports and resolve related field dependencies

--- records_management/models/__init__.py
+++ records_management/models/__init__.py
@@ -3,8 +3,13 @@
 from . import (
     portal_request,
     records_container,
+    records_container_type,
     records_department,
     records_document,
+    records_document_type,
     records_location,
+    records_retention_policy,
     rm_module_configurator,
+    shredding_service,
+    temp_inventory,
 )
