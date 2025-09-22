5be0db854e10c9a812ef2bc06f31b69f8d4326aa
John75SunCity <johncope75@yahoo.com>
2025-09-22 19:18:23 -0400

    fix:

--- records_management/static/src/js/trailer_visualization.js
+++ records_management/static/src/js/trailer_visualization.js
@@ -7,10 +7,4 @@ import { TruckProgressWidget } from "./truck_widget";
 registry.category("components").add("TruckProgressWidget", TruckProgressWidget);
 
 /* Trailer visualization JavaScript for interactive truck loading display */
-
-odoo.define('records_management.trailer_visualization', function (require) {
-    'use strict';
-    
-    // Additional trailer visualization widgets will be implemented here
-    
-});
+// Additional trailer visualization widgets can be added here using @odoo-module style
