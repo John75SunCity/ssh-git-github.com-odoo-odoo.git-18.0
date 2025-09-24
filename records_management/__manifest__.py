c89d8674bb4048ad54d29102e6fb2efde1549a5f
John75SunCity <johncope75@yahoo.com>
2025-09-24 17:19:18 -0500

    feat: re-enable previously disabled ESM modules in manifest; enhance report templates with improved structure and error handling; add report page wrapper audit script for diagnosing layout issues

--- records_management/__manifest__.py
+++ records_management/__manifest__.py
@@ -292,15 +292,15 @@
             "records_management/static/src/js/trailer_visualization.js",
             "records_management/static/src/js/truck_widget.js",
             # Temporarily disabled custom visualization/search ESM modules pending dependency resolution
-            # "records_management/static/src/js/intelligent_search.js",
-            # "records_management/static/src/js/system_flowchart_view.js",
-            # "records_management/static/src/js/customer_portal_diagram_view.js",
+            "records_management/static/src/js/intelligent_search.js",
+            "records_management/static/src/js/system_flowchart_view.js",
+            "records_management/static/src/js/customer_portal_diagram_view.js",
             "records_management/static/src/xml/map_widget.xml",
             "records_management/static/src/xml/trailer_visualization.xml",
             # Templates for disabled modules also commented to avoid unresolved template references
-            # "records_management/static/src/xml/intelligent_search_templates.xml",
-            # "records_management/static/src/xml/system_flowchart_templates.xml",
-            # "records_management/static/src/xml/customer_portal_diagram_templates.xml"
+            "records_management/static/src/xml/intelligent_search_templates.xml",
+            "records_management/static/src/xml/system_flowchart_templates.xml",
+            "records_management/static/src/xml/customer_portal_diagram_templates.xml"
         ],
         "web.assets_frontend": [
             "records_management/static/src/lib/vis/vis-network.min.css",
