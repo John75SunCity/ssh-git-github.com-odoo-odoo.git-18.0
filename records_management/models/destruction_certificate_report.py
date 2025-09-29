73ba1f78152c2cd62f1d1550897ae3e4740f6d05
John75SunCity <johncope75@yahoo.com>
2025-09-28 20:24:56 -0600

    fix(reports): Fix missing destruction certificate template causing test failures
    - Fixed XML template ID mismatch: destruction_certificate_template -> destruction_cert_template
    - Fixed Python report model name: report.rm.* -> report.records_management.*
    - Updated report model to target correct model (shredding.service vs destruction.certificate)
    - All report references now consistently point to existing template
    - Resolves: ValueError: External ID not found in the system: records_management.destruction_cert_template

--- records_management/models/destruction_certificate_report.py
+++ records_management/models/destruction_certificate_report.py
@@ -2,23 +2,23 @@ from odoo import models, fields, api
 
 
 class DestructionCertificateReport(models.AbstractModel):
-    _name = 'report.rm.destruction_cert_template'
+    _name = 'report.records_management.destruction_cert_template'
     _description = 'Destruction Certificate Report'
 
     @api.model
     def _get_report_values(self, docids, data=None):
         """Get report values for destruction certificate."""
-        docs = self.env['destruction.certificate'].browse(docids)
+        docs = self.env['shredding.service'].browse(docids)
         return {
             'doc_ids': docids,
-            'doc_model': 'destruction.certificate',
+            'doc_model': 'shredding.service',
             'docs': docs,
             'data': data,
         }
 
 
 class ReportDestructionCertificateTemplate(models.AbstractModel):
-    _name = 'report.rm.report_destruction_cert_template'
+    _name = 'report.records_management.report_destruction_certificate_template'
     _description = 'Report Destruction Certificate Template'
 
     @api.model
@@ -27,7 +27,7 @@ class ReportDestructionCertificateTemplate(models.AbstractModel):
         docs = self.env['destruction.certificate'].browse(docids)
         return {
             'doc_ids': docids,
-            'doc_model': 'destruction.certificate',
+            'doc_model': 'destruction.certificate', 
             'docs': docs,
             'data': data,
         }
