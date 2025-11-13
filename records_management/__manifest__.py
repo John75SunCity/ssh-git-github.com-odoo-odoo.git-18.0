271027fd8c9dd690e9cf519cc8ff9a25549acd8e
John75SunCity <johncope75@yahoo.com>
2025-11-12 21:16:29 -0700

    feat: Move demo data to data section for automatic loading
    - Moved all 7 demo files from 'demo' to 'data' section
    - Demo data now loads automatically on module installation
    - Includes 98 customer containers, billing examples, field labels, etc.
    - No longer requires 'Load Demonstration Data' checkbox during install

--- records_management/__manifest__.py
+++ records_management/__manifest__.py
@@ -679,9 +679,8 @@
         "templates/portal_mobile_template.xml",
         "templates/portal_mobile_inventory_template.xml",
         "templates/portal_mobile_export.xml",
-        "templates/portal_mobile_settings.xml"
-    ],
-    "demo": [
+        "templates/portal_mobile_settings.xml",
+        # Demo Data (moved to data section to auto-load on installation)
         "demo/customer_inventory_demo.xml",
         "demo/advanced_billing_demo.xml",
         "demo/model_records_demo.xml",
@@ -689,6 +688,8 @@
         "demo/intelligent_search_demo_data.xml",
         "demo/naid_demo_certificates.xml",
         "demo/records_config_mail_templates_data.xml"
+    ],
+    "demo": [
     ],
     "pre_init_hook": "pre_init_hook",
     "post_init_hook": "post_init_hook",
