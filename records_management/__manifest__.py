e1a4ab3a596433725384cf081168fd8c5f68add0
John75SunCity <johncope75@yahoo.com>
2025-11-15 13:38:08 -0700

    feat: Add new card-based menu for customer portal on /my/home page

--- records_management/__manifest__.py
+++ records_management/__manifest__.py
@@ -663,6 +663,7 @@
         "templates/portal_dashboards.xml",
         "templates/portal_home_preconfigured.xml",
         # "templates/portal_consolidated_menu.xml",  # DISABLED: XPath issues in Odoo 18, reorganize menu later
+        "templates/portal_my_menu.xml",  # NEW: Simple card-based menu on /my/home page
         "templates/portal_access_templates.xml",
         "templates/portal_errors.xml",
         # Inventory & Container Templates
